4b196f695ab63a5829f8b24d18e636d7b8c01049
manish-4007 <manishrai709130@gmail.com>
2022-09-10 08:10:56 +0530

    The web App is ready

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import imp
 import pickle
 from flask import Flask,request, app, jsonify,url_for,render_template
 import numpy as np
-import pandas as np
+import pandas as pd
 
 app = Flask(__name__)
 model = pickle.load(open('boston_prediction.pkl', 'rb'))
@@ -20,6 +21,15 @@ def predict_api():
     output = model.predict(new_data)
     print(output)
     return jsonify(output[0])
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = model.predict(final_input)
+    print(output[0])
+    return render_template("home.html", prediction_text = "The Predicted price of the House is  ${}.".format(round(output[0]*1000,2)))
 
 if __name__ == "__main__":
     app.run(debug=True)
